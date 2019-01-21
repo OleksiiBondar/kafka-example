9fabfeacb33b5fe2a0eecd3f0125e26dbf2a1382
Oleksii Bondar <alexBondar.kha@gmail.com>
2019-01-20 20:01:03 -0800

    update

--- src/main/java/kafkaExample/Consumer.java
+++ src/main/java/kafkaExample/Consumer.java
@@ -8,25 +8,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
 public class Consumer {
-    
+
     private final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-    
+
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringDeserializer");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-consumer-group");
-        props.put(ConsumerConfig.METRIC_REPORTER_CLASSES_CONFIG, "io.micrometer.core.instrument.binder.kafka.MicrometerKafkaMetrics");
-        KafkaConsumer consumer = new KafkaConsumer(props);
+        props.put(ConsumerConfig.METRIC_REPORTER_CLASSES_CONFIG,
+                "io.micrometer.core.instrument.binder.kafka.KafkaConsumerApiMetrics");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
         consumer.subscribe(Arrays.asList("odd", "even"));
         int counter = 0;
         while (counter <= 10) {
-            ConsumerRecords<String, String> recs = consumer.poll(10);
+            ConsumerRecords<String, String> recs = consumer.poll(Duration.ofMillis(100));
             if (recs.count() == 0) {
             } else {
                 for (ConsumerRecord<String, String> rec : recs) {
@@ -35,5 +39,6 @@ public class Consumer {
             }
             counter++;
         }
+        consumer.close();
     }
 }
--- src/main/java/kafkaexample/Producer.java
+++ src/main/java/kafkaexample/Producer.java
@@ -18,7 +18,7 @@ public class Producer {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        props.put(ConsumerConfig.METRIC_REPORTER_CLASSES_CONFIG, "io.micrometer.core.instrument.binder.kafka.MicrometerKafkaMetrics");
+        props.put(ConsumerConfig.METRIC_REPORTER_CLASSES_CONFIG, "io.micrometer.core.instrument.binder.kafka.KafkaProducerApiMetrics");
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
         for (int i = 0; i < 10; i++) {
             ProducerRecord<String, String> data;
